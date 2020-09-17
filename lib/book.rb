8d4dfab38a0de032e697dad275a76cbfb4eb66a7
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-09-17 19:33:07 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,40 @@
+class Book 
+  def initialize(title)
+    @title = title
+  end 
+  
+  def title #reader
+    @title
+  end
+  
+  def author= (author) #writer, setter
+    @author = author
+  end
+  
+  def author #reader, getter
+    @author
+  end
+  
+  def page_count= (num) #writer
+    @page_count = num
+  end
+  
+  def page_count #reader
+    @page_count
+  end
+  
+  def genre= (genre)
+    @genre = genre
+  end
+  
+  def genre
+    @genre
+  end
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
 
-
+book = Book.new("And Then There Were None")
+book.turn_page
